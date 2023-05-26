5d715177f896e21ca02cc5eccb3677e33897670b
Piotr Galar <piotr.galar@gmail.com>
2023-05-26 14:14:14 +0200

    Update fix-yaml-config.ts

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -2,20 +2,43 @@ import 'reflect-metadata'
 import {Repository} from '../resources/repository'
 import {addFileToAllRepos} from './shared/add-file-to-all-repos'
 import {format} from './shared/format'
+import {protectDefaultBranches} from './shared/protect-default-branches'
+import {setPropertyInAllRepos} from './shared/set-property-in-all-repos'
 
-// exclude fclibp2p-zhi because it is not initialised
-const uninitialisedRepos = [
-  'fclibp2p-zhi',
-  'universal-connectivity'
-]
+function isInitialised(repository: Repository) {
+  return ![
+    'fclibp2p-zhi',
+    'universal-connectivity'
+  ].includes(repository.name)
+}
+
+function isJS(repository: Repository) {
+  return repository.name.startsWith('js-libp2p')
+}
+
+function isPublic(repository: Repository) {
+  return repository.visibility === 'public'
+}
+
+protectDefaultBranches()
 addFileToAllRepos(
   '.github/workflows/stale.yml',
   '.github/workflows/stale.yml',
-  (repository: Repository) => !uninitialisedRepos.includes(repository.name)
+  isInitialised
 )
 addFileToAllRepos(
   '.github/workflows/semantic-pull-request.yml',
   '.github/workflows/semantic-pull-request.yml',
-  (repository: Repository) => repository.name.startsWith('js-libp2p')
+  r => isInitialised(r) && isJS(r)
+)
+setPropertyInAllRepos(
+  'secret_scanning',
+  true,
+  r => isInitialised(r) && isPublic(r)
+)
+setPropertyInAllRepos(
+  'secret_scanning_push_protection',
+  true,
+  r => isInitialised(r) && isPublic(r)
 )
 format()
